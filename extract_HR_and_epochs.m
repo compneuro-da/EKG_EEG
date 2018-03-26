970e18fd9c2066f174db4cf1f5ec1bb7422f4b8b
liesaravijts <liesa.ravijts@ugent.be>
2018-03-26 11:17:50 +0200

    changed the load directory

--- extract_HR_and_epochs.m
+++ extract_HR_and_epochs.m
@@ -1,4 +1,4 @@
-load('C:\Users\dmarinaz\Documents\code\DREAMER\DREAMER.mat') %load the full dataset, change directory accordingly
+load('\\Client\C$\Users\Liesa\Desktop\EEG\data\DREAMER.mat') %load the full dataset, change directory accordingly
 ecg=cell2mat(DREAMER.Data{1,1}.ECG.baseline(1)); %load the ecg for a given subject and condition
 ecg(:,2)=[]; %use only one ecg channel
 ECG_srate=DREAMER.ECG_SamplingRate;
@@ -28,7 +28,7 @@ HR_EEG_epochs=zeros(nchan,epoch_length,n_epochs); % vector of epochs, same forma
 for i_epochs=1:n_epochs
     HR_EEG_epochs(:,:,i_epochs)=eeg(HB_points(i_epochs):HB_points(i_epochs)+epoch_length-1,:)'; % transpose since channels have to be the first dimension
 end
-avg_HRP=squeeze(mean(HR_EEG_epochs,3)); % average to find the HRP
+avg_HRP=squeeze(mean(HR_EEG_epochs,3)); % average to find the HRP on 3rd dimension
 time_epoch=(0:epoch_length-1)/EEG_srate;
 figure;
 for i_chan=1:14
