d6c7ebf705172db852af1e43124cc551adb48a6e
danielemarinazzo <daniele.marinazzo@gmail.com>
2018-09-10 14:47:38 +0200

    added subfunction

---
+++ PS2H.m
@@ -0,0 +1,21 @@
+function WS  = PS2H( PS, ECG_srate )
+b=fir1(48,[0.8 2.8]/(ECG_srate/2));
+% PS = filtfilt(b,1,PS);
+lambda_max = l1tf_lambdamax(PS);
+[trend,~] = l1tf(PS, 0.0001*lambda_max);
+PS_d=PS-trend;
+PS_d = filtfilt(b,1,PS_d);
+PS_dz=zscore(PS_d);
+%PS_dz=PS;
+%dPS=(diff(PS_dz,1));
+%bdPS=dPS>0;
+ddPS=(diff(PS_dz,2));
+%ddPS(ddPS<0)=0;
+%WS=bdPS(2:end).*ddPS;
+WS=zscore(ddPS);
+WS = zscore(filtfilt(b,1,WS));
+% if min(WS)<0
+%     WS=WS-min(WS);
+% end
+end
+
