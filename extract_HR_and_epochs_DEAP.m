b90caf5bf6b4790e3b622ae57a1ebe99d35dd863
danielemarinazzo <daniele.marinazzo@gmail.com>
2018-09-12 12:40:30 +0200

    shifted peak locations

--- extract_HR_and_epochs_DEAP.m
+++ extract_HR_and_epochs_DEAP.m
@@ -1,33 +1,38 @@
 clear;clc;close all
-load('C:\Users\dmarinaz\Documents\code\DEAP\preprocessed\s01.mat') %load the full dataset, change directory accordingly
+load DEAP_chnames
+sub=1; %subject index, you can loop on this
+load(['C:\Users\dmarinaz\Documents\code\DEAP\preprocessed\s' num2str(sub,'%02.0f') '.mat']) %load the full dataset, change directory accordingly
 % see http://www.eecs.qmul.ac.uk/mmv/datasets/deap/readme.html
 % data	40 x 40 x 8064	video/trial x channel x data
 % labels	40 x 4	video/trial x label (valence, arousal, dominance, liking)
+% channels have a different order in subs 23 to 32, here the index to
+% reorder them. We choose the Twente order and name.
 itrial=1; % here you choose a trial, eventually you can loop on it
 ECG_srate=128;
 time_start=-.2; % time pre heartbeat (200 ms)
-time_end=.3; %time post heartbeat (300 ms)
-PS=squeeze(data(itrial,39,3*ECG_srate+1:end)); % plethysmograph; let's discard the 3 seconds baseline
+time_end=.6; %time post heartbeat (300 ms)
+PS=squeeze(data(itrial,39,:)); % plethysmograph;
 time_ECG=(0:length(PS)-1)/ECG_srate;
-%figure;plot(time_ECG,PS);title('plethysmograph signal');
 %% now let's detect R peaks from plethysmogram, we need to detrend the signal, and differentiate it twice
-ddPS  = PS2H( PS, ECG_srate );
+[ddPS, PS]  = PS2H( PS, ECG_srate );
 %%
 [pks,locs] = findpeaks(ddPS,ECG_srate,'MinPeakDistance',.6,'MinPeakHeight',.1);% look for peaks at least .4 sec away
-figure;plot(time_ECG(2:end-1),ddPS);hold on;scatter(locs,pks,'r');title('see if we got the peaks right')
 
 IBI=diff(locs); %interbeat interval
 min_IBI=min(IBI); %let's see what the shortest one is
 locs(1)=[];locs(end)=[]; % let's discard the first and last epoch
+IBI=diff(locs); %interbeat interval
+locs=locs-mean(IBI)/5; % shift locations back in time since the second derivative of the Plethysmogram sugnal comes later than the R peak
 n_epochs=length(locs);
 %% now let's load the EEG
 EEG_srate=128;
 points_start=abs(floor(time_start*EEG_srate)); %number of points before HB
 points_end=abs(floor(time_end*EEG_srate)); %number of points after HB
-eeg=squeeze(data(itrial,1:32,3*EEG_srate+1:end))'; % let's discard the 3 seconds baseline
+eeg=squeeze(data(itrial,1:32,:))'; 
+eeg=zscore(eeg);
 [npoints, nchan]=size(eeg);
 %epoch_length=floor(min_IBI*EEG_srate); % the length of the heartbeat evoked potential is equal to the shortest interbeat interval
-epoch_length=floor((time_end-time_start)*EEG_srate);
+epoch_length=points_start+points_end;
 time_EEG=(0:length(eeg)-1)/EEG_srate;
 
 %% extract the epochs
@@ -37,10 +42,17 @@ HR_EEG_epochs=zeros(nchan,epoch_length,n_epochs); % vector of epochs, same forma
 for i_epochs=1:n_epochs
     HR_EEG_epochs(:,:,i_epochs)=eeg(HB_points(i_epochs)-points_start:HB_points(i_epochs)+points_end-1,:)'; % transpose since channels have to be the first dimension
 end
-avg_HRP=squeeze(mean(HR_EEG_epochs,3)); % average to find the HRP
-time_epoch=[-.2:1/EEG_srate:.3-1/EEG_srate];
+avg_HRP=squeeze(mean(HR_EEG_epochs,3)); % average HRP
+std_HRP=squeeze(std(HR_EEG_epochs,[],3)); % std of the the HRP
+time_epoch=time_start:1/EEG_srate:time_end;
+if length(time_epoch)>epoch_length
+    time_epoch=time_start:1/EEG_srate:time_end-1/EEG_srate;
+end
+
+% the lines below are if you want to plot all the EEG channels
 figure;
 for i_chan=1:nchan
-    subplot(6,6,i_chan);plot(time_epoch,avg_HRP(i_chan,:));% to add the channel name we should first define the labels
-    xlim([time_start,time_end]);
+    subplot(4,8,i_chan);plot(time_epoch,avg_HRP(i_chan,:));
+    xlim([time_start,time_end]);ylim([min(min(avg_HRP)) max(max(avg_HRP))]);
+    title(ch_labels{i_chan})
 end
