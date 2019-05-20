a4b1d81d5c7fb00e9d57625d88e9ec515a085834
Liesa Ravijts <37576046+liesaravijts@users.noreply.github.com>
2019-05-20 09:44:13 +0200

    Update calc_temp_inf_HEP_DEAP.m

--- calc_temp_inf_HEP_DEAP.m
+++ calc_temp_inf_HEP_DEAP.m
@@ -1,7 +1,8 @@
 clear;clc;close all
 load DEAP_chnames
 isub = 1;
-for isub = 1%:32 % subject index
+
+%for isub = 1:32 % subject index
     load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_HEP.mat']) % load data, change directory accordingly
     % HB_EEG_epochs  HB/trial x time x channel
     % ratings        HB/trial x label (valence, arousal, dominance, liking)
@@ -13,47 +14,77 @@ for isub = 1%:32 % subject index
     srate = 128;
     time = time_start:1/srate:time_end-1/srate;
     
-    hep = squeeze(mean(dat,1));
-    plot(time,hep) 
-    
-    %% copula transform and MI
-    cdat = copnorm(dat);
-    crat = copnorm(ratings);
-    
-    I = zeros(1,ntime);
-    for t=1:ntime
-        I(t) = mi_gg(cdat(:,t),crat(:,1),true,true);
-    end
-    plot(time,I)
-    
-    %% cross-temporal interaction information
-    t1 = 1;
-    t2 = 2;
-    Ijoint = mi_gg([cdat(:,t1) cdat(:,t2)],crat(:,1),true,true);
-   
+    % time window we are interested in
     timewindow = (time>.2) & (time<.6);
     time_int = time(timewindow);
     ntime_int = length(time_int);
     
-    cdat = copnorm(dat(:,timewindow));
-    I = I(timewindow);
+    %hep = squeeze(mean(dat,1));
+    %plot(time,hep) 
     
-    I_int = zeros(ntime_int,ntime_int);
-    for t1=1:ntime_int
-        for t2=(t1+1):ntime_int
-            Ijoint = mi_gg([cdat(:,t1) cdat(:,t2)],crat(:,1),true,true);
-            I_int(t1,t2) = Ijoint - I(t1) - I(t2);
+    %% copula transform and MI
+    cdat = copnorm(dat(:,timewindow));
+    crat = copnorm(ratings);
+    
+    MI = zeros(4,ntime_int);
+    for r=1:4 % loop over 4 labels
+        for t=1:ntime_int
+            MI(r,t) = mi_gg(cdat(:,t),crat(:,r),true,true);
         end
     end
-    I_int = I_int + I_int';
     
-    figure
-    imagesc(time_int,time_int,I_int)
-    colormap parula
-    colorbar
-    axis square
-    xlabel('Time (ms)')
-    ylabel('Time (ms)')
-    title('Interaction information (bits)')
+    %% plot MI
+    subplot(4,1,1)
+    plot(time_int,MI(1,:))
+    title('valence')
+    subplot(4,1,2)
+    plot(time_int,MI(2,:))
+    title('arousal')
+    subplot(4,1,3)
+    plot(time_int,MI(3,:))
+    title('dominance')
+    subplot(4,1,4)
+    plot(time_int,MI(4,:))
+    title('liking')
     
-end
+    %% cross-temporal interaction information
+    t1 = 1;
+    t2 = 2;
+    
+    II(:,:,4) = zeros(ntime_int,ntime_int);
+    for r=1:4
+        for t1=1:ntime_int
+            MI1(r,t1) = MI(r,t1);
+            for t2=(t1+1):ntime_int
+                JMI(r,:) = mi_gg([cdat(:,t1) cdat(:,t2)],crat(:,r),true,true);
+                MI2(r,t2) = MI(r,t2);
+                II(t1,t2,r) = JMI(r,:) - MI1(r,t1) - MI2(r,t2);
+            end
+        end
+        II(:,:,r) = II(:,:,r) + II(:,:,r)';
+    end
+    
+    plottitle = sprintf('Interaction information (bits) - sub %02.0f', isub);
+    suptitle(plottitle)
+    for r=1:4
+        subplot(2,2,r);
+        imagesc(time_int,time_int,II(:,:,r))
+        colormap parula
+        colorbar
+        axis square
+        xlabel('Time (ms)')
+        ylabel('Time (ms)')
+        if r==1
+            title('valence')
+        elseif r==2
+            title('arousal')
+        elseif r==3
+            title('dominance')
+        else
+            title('liking')
+        end
+    end
+    
+    %% partial information decomposition
+    
+%end
