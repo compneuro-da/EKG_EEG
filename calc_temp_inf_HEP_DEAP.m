7ce2be62ba07b5133405e49b1498b2ff1149335c
Liesa Ravijts <37576046+liesaravijts@users.noreply.github.com>
2019-07-08 09:10:53 +0200

    Update calc_temp_inf_HEP_DEAP.m

--- calc_temp_inf_HEP_DEAP.m
+++ calc_temp_inf_HEP_DEAP.m
@@ -1,53 +1,39 @@
 clear;clc;close all
 load DEAP_chnames
-isub = 1;
-
-%for isub = 1:32 % subject index
+chan = 24; % choose an electrode (Cz)
+time_start = -.2;
+time_end = .6;
+srate = 128;
+time_epoch = time_start:1/srate:time_end-1/srate;
+% time window we are interested in
+timewindow = (time_epoch>.2) & (time_epoch<.6);
+time_info = time_epoch(timewindow);
+ntime_info = length(time_info);
+%%
+for isub = 1:32
+    disp(isub);
     load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_HEP.mat']) % load data, change directory accordingly
-    % HB_EEG_epochs  HB/trial x time x channel
-    % ratings        HB/trial x label (valence, arousal, dominance, liking)
-    chan = 24; % Cz
-    dat = HB_EEG_epochs(:,:,chan); % HB epochs for Cz electrode
-    [ntrls,ntime] = size(dat);
+    % HB_EEG_epochs  HBs x data points x EEG channels
+    % ratings        HBs x labels (valence, arousal, dominance, liking)
+    %% initialization
+    dat = HB_EEG_epochs(:,:,chan);
+    [ntrls,ntime_epoch] = size(dat);
     nlbls = size(ratings,2);
-    time_start = -.2;
-    time_end = .6;
-    srate = 128;
-    time = time_start:1/srate:time_end-1/srate;
     
-    %hep = squeeze(mean(dat,1));
-    %plot(time,hep) 
-    
-    % time window we are interested in
-    timewindow = (time>.2) & (time<.6);
-    time_info = time(timewindow);
-    ntime_info = length(time_info);
-    
+    hep = squeeze(mean(dat(:,timewindow),1));
+    %plot(time_info,hep)
+
     %% copula transform and calculate mutual information (MI) at each time point
     cdat = copnorm(dat(:,timewindow));
     cratings = copnorm(ratings);
-    
+   
     MI = zeros(nlbls,ntime_info);
     for l=1:nlbls % loop over 4 labels
         for t=1:ntime_info
             MI(l,t) = mi_gg(cdat(:,t),cratings(:,l),true,true);
         end
     end
-    
-    %% plot MI
-    subplot(4,1,1)
-    plot(time_info,MI(1,:))
-    title('valence')
-    subplot(4,1,2)
-    plot(time_info,MI(2,:))
-    title('arousal')
-    subplot(4,1,3)
-    plot(time_info,MI(3,:))
-    title('dominance')
-    subplot(4,1,4)
-    plot(time_info,MI(4,:))
-    title('liking')
-    
+
     %% cross-temporal interaction information (II)
     JMI = zeros(nlbls,1);
     II = zeros(ntime_info,ntime_info,nlbls);
@@ -55,41 +41,23 @@ isub = 1;
         for t1=1:ntime_info
             for t2=(t1+1):ntime_info
                 JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),true,true);
-                II(t1,t2,l) = JMI(l,:) - MI(l,t1) - MI(l,t2);     
+                II(t1,t2,l) = JMI(l,:) - MI(l,t1) - MI(l,t2);
             end
         end
         II(:,:,l) = II(:,:,l) + II(:,:,l)';
     end
-    
-    %% plot II
-    plottitle = sprintf('Interaction information (bits) - sub %02.0f', isub);
-    suptitle(plottitle)
-    for l=1:nlbls
-        subplot(2,2,l);
-        imagesc(time_info,time_info,II(:,:,l))
-        colormap parula
-        colorbar
-        axis square
-        xlabel('Time (ms)')
-        ylabel('Time (ms)')
-        if l==1
-            title('valence')
-        elseif l==2
-            title('arousal')
-        elseif l==3
-            title('dominance')
-        else
-            title('liking')
-        end
-    end
-    
+    II(II==0)=NaN;
+  
     %% partial information decomposition (PID)
     % see Faes et al., 2017; Ince, 2017
-    % II = JMI - MI1 - MI2;
+    % JMI = gcmi_cc(t,[d1 d2]); % t = target, d1 = source, d2 = source
+    % MI1 = gcmi_cc(t,d1);
+    % MI2 = gcmi_cc(t,d2);
     % RED = min(MI1,MI2);
     % U1 = MI1 - RED;
     % U2 = MI2 - RED;
     % SYN = JMI - RED - U1 - U2;
+    % II = JMI - MI1 - MI2;
     
     JMI = zeros(nlbls,1);
     II = zeros(ntime_info,ntime_info,nlbls);
@@ -102,7 +70,7 @@ isub = 1;
             for t2=(t1+1):ntime_info
                 JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),true,true);
                 II(t1,t2,l) = JMI(l,:) - MI(l,t1) - MI(l,t2);
-            
+         
                 RED(t1,t2,l) = min(MI(l,t1),MI(l,t2));
                 U1(l,t1) = MI(l,t1) - RED(t1,t2,l);
                 U2(l,t2) = MI(l,t2) - RED(t1,t2,l);
@@ -113,53 +81,11 @@ isub = 1;
         SYN(:,:,l) = SYN(:,:,l) + SYN(:,:,l)';
         RED(:,:,l) = RED(:,:,l) + RED(:,:,l)';
     end
-    
-    %% plot PI
+    II(II==0)=NaN;
     SYN(SYN==0)=NaN;
     RED(RED==0)=NaN;
     
-    plottitle = sprintf('Partial information (synergy) - sub %02.0f', isub);
-    suptitle(plottitle)
-    for l=1:nlbls
-        subplot(2,2,l);
-        imagesc(time_info,time_info,SYN(:,:,l))
-        colormap parula
-        colorbar
-        axis square 
-        xlabel('Time (ms)')
-        ylabel('Time (ms)')
-        title('Synergy (bits)')
-        if l==1
-            title('valence')
-        elseif l==2
-            title('arousal')
-        elseif l==3
-            title('dominance')
-        else
-            title('liking')
-        end
-    end
-    %%
-    plottitle = sprintf('Partial information (redundancy) - sub %02.0f', isub);
-    suptitle(plottitle)
-    for l=1:nlbls
-        subplot(2,2,l);
-        imagesc(time_info,time_info,RED(:,:,l))
-        colormap parula
-        colorbar
-        axis square 
-        xlabel('Time (ms)')
-        ylabel('Time (ms)')
-        title('Synergy (bits)')
-        if l==1
-            title('valence')
-        elseif l==2
-            title('arousal')
-        elseif l==3
-            title('dominance')
-        else
-            title('liking')
-        end
-    end
-    
-%end
+    %% save results
+    %savefile = ['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_I.mat'];
+    %save(savefile,'hep','MI','II','SYN','RED');
+end
