fc2823dd079e7d7b03248f1add87333c40fd0486
Liesa Ravijts <37576046+liesaravijts@users.noreply.github.com>
2019-07-16 21:31:39 +0200

    calculate temporal information quantities

--- calc_temp_inf_HEP_DEAP.m
+++ calc_temp_inf_HEP_DEAP.m
@@ -1,36 +1,52 @@
 clear;clc;close all
 load DEAP_chnames
-chan = 24; % choose an electrode (Cz)
+chan = 24; % choose an electrode
 time_start = -.2;
 time_end = .6;
 srate = 128;
 time_epoch = time_start:1/srate:time_end-1/srate;
+ntime_epoch = length(time_epoch);
 % time window we are interested in
 timewindow = (time_epoch>.2) & (time_epoch<.6);
 time_info = time_epoch(timewindow);
 ntime_info = length(time_info);
-%%
+
+%% calculate temporal information per subject
 for isub = 1:32
     disp(isub);
-    load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_HEP.mat']) % load data, change directory accordingly
-    % HB_EEG_epochs  HBs x data points x EEG channels
-    % ratings        HBs x labels (valence, arousal, dominance, liking)
-    %% initialization
-    dat = HB_EEG_epochs(:,:,chan);
-    [ntrls,ntime_epoch] = size(dat);
-    nlbls = size(ratings,2);
+    load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_avgHEP.mat']) % load data, change directory accordingly
+    %load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_HEP.mat'])
     
-    hep = squeeze(mean(dat(:,timewindow),1));
-    %plot(time_info,hep)
-
+    if isub == 1
+       avg_HEP_tot = avg_HEP;
+       labels_tot = labels;
+       %HB_trials_tot = HB_trials;
+       %ratings_tot = ratings;
+    else
+       avg_HEP_tot = cat(1,avg_HEP_tot,avg_HEP);
+       labels_tot = cat(1,labels_tot,labels);
+       %HB_trials_tot = cat(1,HB_trials_tot,HB_trials);
+       %ratings_tot = cat(1,ratings_tot,ratings);
+    end
+    
+    %% initialization
+    dat = avg_HEP(:,timewindow,chan);
+    %dat = HB_trials(:,timewindow,chan);
+    [ntrls,~] = size(dat);
+    
+    HEP = squeeze(mean(dat,1));
+    %plot(time_info,HEP)
+    
     %% copula transform and calculate mutual information (MI) at each time point
-    cdat = copnorm(dat(:,timewindow));
-    cratings = copnorm(ratings);
+    cdat = copnorm(dat);
+    cratings = copnorm(labels);
+    %cratings = copnorm(ratings);
+    nlbls = size(cratings,2);
    
     MI = zeros(nlbls,ntime_info);
     for l=1:nlbls % loop over 4 labels
         for t=1:ntime_info
-            MI(l,t) = mi_gg(cdat(:,t),cratings(:,l),true,true);
+            MI(l,t) = mi_gg(cdat(:,t),cratings(:,l),false);
         end
     end
 
@@ -40,7 +56,7 @@ for isub = 1:32
     for l=1:nlbls
         for t1=1:ntime_info
             for t2=(t1+1):ntime_info
-                JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),true,true);
+                JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),false);
                 II(t1,t2,l) = JMI(l,:) - MI(l,t1) - MI(l,t2);
             end
         end
@@ -49,15 +65,15 @@ for isub = 1:32
     II(II==0)=NaN;
   
     %% partial information decomposition (PID)
-    % see Faes et al., 2017; Ince, 2017
-    % JMI = gcmi_cc(t,[d1 d2]); % t = target, d1 = source, d2 = source
-    % MI1 = gcmi_cc(t,d1);
-    % MI2 = gcmi_cc(t,d2);
-    % RED = min(MI1,MI2);
-    % U1 = MI1 - RED;
-    % U2 = MI2 - RED;
-    % SYN = JMI - RED - U1 - U2;
-    % II = JMI - MI1 - MI2;
+    % see Faes, Marinazzo, & Stramaglia (2017)
+    % JMI = gcmi_cc(t,[d1 d2]) with t = target, d1 = source, d2 = source
+    % MI1 = gcmi_cc(t,d1)
+    % MI2 = gcmi_cc(t,d2)
+    % RED = min(MI1,MI2)
+    % U1 = MI1 - RED
+    % U2 = MI2 - RED
+    % SYN = JMI - RED - U1 - U2
+    % II = JMI - MI1 - MI2
     
     JMI = zeros(nlbls,1);
     II = zeros(ntime_info,ntime_info,nlbls);
@@ -68,13 +84,13 @@ for isub = 1:32
     for l=1:nlbls
         for t1=1:ntime_info
             for t2=(t1+1):ntime_info
-                JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),true,true);
+                JMI(l,:) = mi_gg([cdat(:,t1) cdat(:,t2)],cratings(:,l),false);
                 II(t1,t2,l) = JMI(l,:) - MI(l,t1) - MI(l,t2);
          
                 RED(t1,t2,l) = min(MI(l,t1),MI(l,t2));
                 U1(l,t1) = MI(l,t1) - RED(t1,t2,l);
                 U2(l,t2) = MI(l,t2) - RED(t1,t2,l);
-                SYN(t1,t2,l) = JMI(l,:) - RED(t1,t2,l) - U1(l,t1) - U2(l,t2);
+                SYN(t1,t2,l) = JMI(l,:) - U1(l,t1) - U2(l,t2) - RED(t1,t2,l);
             end
         end
         II(:,:,l) = II(:,:,l) + II(:,:,l)';
@@ -86,28 +102,68 @@ for isub = 1:32
     RED(RED==0)=NaN;
     
     %% save results
-    %savefile = ['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_I.mat'];
-    %save(savefile,'hep','MI','II','SYN','RED');
+    savefile = ['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_avgHEP_I.mat'];
+    %savefile = ['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_HEP_I.mat'];
+    save(savefile,'HEP','MI','II','SYN','RED');
 end
 
-%% calculate averages
-for isub = 1:32
-    load(['\\client\d$\Users\Liesa\Documents\Universiteit Gent\Theoretische en experimentele psychologie\MA05\05 J\5 Masterproef II\DEAP\preprocessed\s' num2str(isub,'%02.0f') '_I.mat'])
-    if isub == 1
-       hep_tot = hep;
-       MI_tot = MI;
-       II_tot = II;
-       RED_tot = RED;
-       SYN_tot = SYN;
-    else
-       hep_tot = cat(1,hep_tot,hep);
-       MI_tot = cat(3,MI_tot,MI);
-       II_tot = cat(4,II_tot,II);
-       RED_tot = cat(4,RED_tot,RED);
-       SYN_tot = cat(4,SYN_tot,SYN);
+%% calculate overall temporal information
+dat_tot = avg_HEP_tot(:,timewindow,chan);
+%dat_tot = HB_trials_tot(:,timewindow,chan);
+[ntrls_tot,~] = size(dat_tot);
+
+HEP_tot = squeeze(mean(dat_tot,1));
+%plot(time_info,HEP_tot)
+
+% copula transform and calculate mutual information (MI) at each time point
+cdat_tot = copnorm(dat_tot);
+cratings_tot = copnorm(labels_tot);
+%cratings_tot = copnorm(ratings_tot);
+
+MI_tot = zeros(nlbls,ntime_info);
+for l=1:nlbls % loop over 4 labels
+    for t=1:ntime_info
+        MI_tot(l,t) = mi_gg(cdat_tot(:,t),cratings_tot(:,l),false);
     end
 end
 
-II_avg = squeeze(mean(II_tot,4));
-RED_avg = squeeze(mean(RED_tot,4));
-SYN_avg = squeeze(mean(SYN_tot,4));
+% cross-temporal interaction information (II)
+JMI_tot = zeros(nlbls,1);
+II_tot = zeros(ntime_info,ntime_info,nlbls);
+for l=1:nlbls
+    for t1=1:ntime_info
+        for t2=(t1+1):ntime_info
+            JMI_tot(l,:) = mi_gg([cdat_tot(:,t1) cdat_tot(:,t2)],cratings_tot(:,l),false);
+            II_tot(t1,t2,l) = JMI_tot(l,:) - MI_tot(l,t1) - MI_tot(l,t2);
+        end
+    end
+    II_tot(:,:,l) = II_tot(:,:,l) + II_tot(:,:,l)';
+end
+II_tot(II_tot==0)=NaN;
+  
+% partial information decomposition (PID)
+JMI_tot = zeros(nlbls,1);
+II_tot = zeros(ntime_info,ntime_info,nlbls);
+RED_tot = zeros(ntime_info,ntime_info,nlbls);
+U1_tot = zeros(nlbls,ntime_info);
+U2_tot = zeros(nlbls,ntime_info);
+SYN_tot = zeros(ntime_info,ntime_info,nlbls);
+for l=1:nlbls
+    for t1=1:ntime_info
+        for t2=(t1+1):ntime_info
+            JMI_tot(l,:) = mi_gg([cdat_tot(:,t1) cdat_tot(:,t2)],cratings_tot(:,l),false);
+            II_tot(t1,t2,l) = JMI_tot(l,:) - MI_tot(l,t1) - MI_tot(l,t2);
+     
+            RED_tot(t1,t2,l) = min(MI_tot(l,t1),MI_tot(l,t2));
+            U1_tot(l,t1) = MI_tot(l,t1) - RED_tot(t1,t2,l);
+            U2_tot(l,t2) = MI_tot(l,t2) - RED_tot(t1,t2,l);
+            SYN_tot(t1,t2,l) = JMI_tot(l,:) - U1_tot(l,t1) - U2_tot(l,t2) - RED_tot(t1,t2,l);
+        end
+    end
+    II_tot(:,:,l) = II_tot(:,:,l) + II_tot(:,:,l)';
+    SYN_tot(:,:,l) = SYN_tot(:,:,l) + SYN_tot(:,:,l)';
+    RED_tot(:,:,l) = RED_tot(:,:,l) + RED_tot(:,:,l)';
+end
+II_tot(II_tot==0)=NaN;
+SYN_tot(SYN_tot==0)=NaN;
+RED_tot(RED_tot==0)=NaN;
