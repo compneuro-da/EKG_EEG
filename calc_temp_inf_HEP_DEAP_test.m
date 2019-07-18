66326647ac8c9afcd0a05f9aa62e9c473408bbc3
Liesa Ravijts <37576046+liesaravijts@users.noreply.github.com>
2019-07-18 20:23:41 +0200

    Add files via upload

--- calc_temp_inf_HEP_DEAP_test.m
+++ calc_temp_inf_HEP_DEAP_test.m
@@ -13,8 +13,8 @@ ntime_info = length(time_info);
 
 nperm = 1000; % number of permutations
 pval = 0.05;
-%ntr = 50; % number of triplets
-%pcorr = pval*6/(ntr*(ntr-1)*(ntr-2)); % Bonferroni correction for triplets
+ntr = 50; % number of triplets
+pcorr = pval*6/(ntr*(ntr-1)*(ntr-2)); % Bonferroni correction for triplets
 
 %% load the data
 for isub = 1:32
@@ -32,17 +32,13 @@ dat_tot = avg_HEP_tot(:,timewindow,chan);
 
 %%
 cdat_tot = copnorm(dat_tot);
-cratings_tot = copnorm(labels_tot(:,3));
+cratings_tot = copnorm(labels_tot(:,1));
 
-MI_tot = zeros(1,ntime_info);
-for t=1:ntime_info
-    MI_tot(t) = mi_gg(cdat_tot(:,t),cratings_tot(:,1),false);
-end
-
+h = zeros(ntime_info,ntime_info);
 for t1=1:ntime_info
      for t2=(t1+1):ntime_info
          JMI_tot = mi_gg([cdat_tot(:,t1) cdat_tot(:,t2)],cratings_tot(:,1),false);
-         II_tot = JMI_tot - MI_tot(t1) - MI_tot(t2);
+         II_tot = JMI_tot - mi_gg(cdat_tot(:,t1),cratings_tot(:,1),false) - mi_gg(cdat_tot(:,t2),cratings_tot(:,1),false);
          
          h0 = II_tot;
          n = length(cratings_tot);
@@ -52,10 +48,12 @@ for t1=1:ntime_info
             h(t1,t2) = 0;
          else
              for i = 1:nperm
-                 cratings_tot(:,1) = cratings_tot(randperm(n));
-                 JMI_tot = mi_gg([cdat_tot(:,t1) cdat_tot(:,t2)],cratings_tot(:,1),false);
-                 II_tot = JMI_tot - mi_gg(cdat_tot(:,t1),cratings_tot(:,1),false) - mi_gg(cdat_tot(:,t2),cratings_tot(:,1),false);
-                 hp = II_tot;
+                 cratings_perm(:,1) = cratings_tot(randperm(n));
+                 JMI_perm = mi_gg([cdat_tot(:,t1) cdat_tot(:,t2)],cratings_perm(:,1),false);
+                 II_perm = JMI_perm - mi_gg(cdat_tot(:,t1),cratings_perm(:,1),false) - mi_gg(cdat_tot(:,t2),cratings_perm(:,1),false);
+                 
+                 hp = II_perm;
+                 
                  if hp>h0
                     ncont=ncont+1;
                  elseif hp<h0
@@ -70,3 +68,4 @@ for t1=1:ntime_info
          end
      end
 end
+h = h + h';
